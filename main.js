bc25c636db1be62ecb64050b572b940d285858cf
Amanda <108832388+shvistak@users.noreply.github.com>
2024-03-20 15:08:33 -0300

    2003

---
+++ main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorA11(".botao");
+for (let i=0; i < botoes.lenght; i++){
+botoes[i].onclick = function() {
+    for(let j=0; j<botoes.lenght; j++){
+        botoes[j].classlist.remove ('ativo');
+}
+botoes[i].classlist.add("ativo");
+}

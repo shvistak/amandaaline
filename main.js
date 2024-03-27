404ce628160bfb033dd6061483a4a5e397bca72a
Amanda <108832388+shvistak@users.noreply.github.com>
2024-03-27 15:13:56 -0300

    2703

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const botoes = document.querySelectorA11(".botao");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 for (let i=0; i < botoes.lenght; i++){
 botoes[i].onclick = function() {
     for(let j=0; j<botoes.lenght; j++){
         botoes[j].classlist.remove ('ativo');
+        textos[j].classList.remove("ativo");
 }
-botoes[i].classlist.add("ativo");
+botoes[i].classList.add("ativo");
+textos[i].classlist.add("ativo");
 }

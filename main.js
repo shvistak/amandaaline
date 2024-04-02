bb969bc81f8c119ffe8810944269ea502260c633
Amanda <108832388+shvistak@users.noreply.github.com>
2024-04-02 11:24:58 -0300

    0204

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-02-04T00:00:00")
+let tempoAtual = new Date ();
+contadores[0].textContent = "tempoObjetivo1 - tempoAtual";
+
 for (let i=0; i < botoes.lenght; i++){
 botoes[i].onclick = function() {
     for(let j=0; j<botoes.lenght; j++){
@@ -9,4 +13,5 @@ botoes[i].onclick = function() {
 }
 botoes[i].classList.add("ativo");
 textos[i].classlist.add("ativo");
+}
 }

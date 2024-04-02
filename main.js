a5420b51cc1cd7acf409c9d12d38c0ddf4e1e010
Amanda <108832388+shvistak@users.noreply.github.com>
2024-04-02 11:28:35 -0300

    0204

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-02-04T00:00:00")
+const tempoObjetivo1 = new Date("2024-02-04T00:00:00");
 let tempoAtual = new Date ();
 contadores[0].textContent = "tempoObjetivo1 - tempoAtual";
 

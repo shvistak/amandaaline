5e877421fee1ea902f85d94183640653e5e03e48
Amanda <108832388+shvistak@users.noreply.github.com>
2024-04-09 11:28:30 -0300

    0904

--- main.js
+++ main.js
@@ -16,8 +16,16 @@ const tempoObjetivo2 = new Date("2024-04-04T00:00:00");
 const tempoObjetivo3 = new Date("2024-04-04T00:00:00");
 const tempoObjetivo4 = new Date("2024-04-04T00:00:00");
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-for (let i = 0; i < contadores.length; i++) {}
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+function atualizaCronometro(){
+for (let i = 0; i < contadores.length; i++) {
+contadores[i].textContent = calculaTempo(tempos[i]);
+}
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+comecaCronometro();
 function calculaTempo(tempoObjetivo){
      let tempoAtual =new Date();
      let tempoFinal = tempoObjetivo - tempoAtual;
@@ -30,5 +38,9 @@ function calculaTempo(tempoObjetivo){
      minutos %= 60;
      horas %= 24;
      
+     if (tempoFinal > 0){
      return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+     }else {
+        return "Prazo Finalizado";
+     }
 }

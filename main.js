61cc92d593241498b71bb79dec5444b1a091e36c
Amanda <108832388+shvistak@users.noreply.github.com>
2024-04-16 11:44:02 -0300

    1604

--- main.js
+++ main.js
@@ -17,8 +17,12 @@ const tempoObjetivo3 = new Date("2024-04-04T00:00:00");
 const tempoObjetivo4 = new Date("2024-04-04T00:00:00");
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 function atualizaCronometro(){
+    document.getElementById("dias0"+i).textContent=calculaTempo(tempos[i])[0];
+    document.getElementById("horas0"+i).textContent=calculaTempo(tempos[i])[1];
+    document.getElementById("min0"+i).textContent=calculaTempo(tempos[i])[2];
+    document.getElementById("seg0"+i).textContent=calculaTempo(tempos[i])[3];
 for (let i = 0; i < contadores.length; i++) {
-contadores[i].textContent = calculaTempo(tempos[i]);
+//contadores[i].textContent = calculaTempo(tempos[i]);
 }
 }
 function comecaCronometro(){
@@ -39,8 +43,8 @@ function calculaTempo(tempoObjetivo){
      horas %= 24;
      
      if (tempoFinal > 0){
-     return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+     return [dias,horas,minutos,segundos];
      }else {
-        return "Prazo Finalizado";
+        return [0,0,0,0];
      }
 }
